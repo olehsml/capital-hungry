7e763f237215dc1b3eb3a3d7c530c0fa786dd867
Oleh <66628075+olehsml@users.noreply.github.com>
2022-02-06 04:48:55 -1200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for (let i = 0; i < marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.navitem')
+
+navToggle.addEventListener('click', () => {
+  document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+  link.addEventListener('click', () => {
+    document.body.classList.remove('nav-open');
+  })
+})
